d5cef55dfb13838c76f38120a854348575f21258
prgMgava <prgmgava@hotmail.com>
2022-11-07 19:40:40 -0300

    feature - add new layout

--- src/Components/Calendar/Components/Filter/Filter.tsx
+++ src/Components/Calendar/Components/Filter/Filter.tsx
@@ -21,59 +21,87 @@ export const Filter = ({ setCurrentPriority }: FilterProps) => {
   }
 
   return (
-    <Box
-      style={{ background: "transparent", minHeight: 100 }}
-      mb={1}
-      display={"flex"}
-      gap={2}
-      flexWrap={"wrap"}
-      padding="0 4px"
-    >
+    <>
       <Box
-        component={"span"}
-        key={uuid()}
-        onClick={() => setCurrentPriority(0)}
-        style={{ background: "white", padding: "2px 4px", height: "min-content", cursor: "pointer" }}
-        boxShadow={1}
-        borderRadius={1}
+        style={{ borderBottom: "1px solid #000", marginBottom: "8px", background: "gray" }}
+        display="flex"
+        gap={2}
+        justifyContent={"center"}
+        alignItems="center"
+        pt={"8px"}
+        color="#fff"
       >
-        Todos
+        <Box pb="6px">Todos artistas</Box>
+        <Box pb="6px">Novo Evento</Box>
+        <Box pb="6px">Eventos</Box>
+        <Box pb="6px">Artistas</Box>
       </Box>
-      {labelsProvider.map(label => {
-        if (!label.is_deleted) {
-          return (
-            <Box
-              component={"span"}
-              key={uuid()}
-              onClick={() => setCurrentPriority(label?.id)}
-              style={{ background: label.color, padding: "2px 4px", height: "min-content", cursor: "pointer" }}
-              boxShadow={1}
-              color={hexToRgb(label.color) > 120 ? "#000000" : "#ffffff"}
-              borderRadius={1}
-            >
-              {label.title}
-            </Box>
-          )
-        }
-        return <></>
-      })}
-      <Dialog onClose={handleClose} open={openModal}>
-        <Card style={{ padding: "8px", display: "flex", flexDirection: "column" }}>
-          <Stack>
-            <Box component={"span"} key={uuid()} onClick={() => window.location.reload()} borderRadius={1}>
-              <b>Seja bem vindo.</b>
-              <br />
-              <br /> Hum vi que é primeira vez que você esta acessando ou você acabou de logar nossa agenda por este
-              dispositivo.
-              <br /> É só <b onClick={handleClose}>clicar</b> no botão abaixo que vamos buscar toas as informações para
-              você!
-            </Box>
-          </Stack>
-          <Button onClick={handleClose} variant={"contained"} style={{ marginTop: "32px" }}>
-            Vamos lá!
-          </Button>
-        </Card>
-      </Dialog>
-    </Box>
+
+      <Box style={{ background: "transparent" }} display={"flex"} flexWrap={"wrap"} gap={2} padding="0 4px">
+        <Box
+          onClick={() => setCurrentPriority(0)}
+          display={"flex"}
+          gap="5px"
+          style={{ height: "min-content", width: "130px", cursor: "pointer" }}
+        >
+          <Box
+            key={uuid()}
+            style={{ background: "white", padding: "2px 4px", height: "15px", cursor: "pointer", width: "15px" }}
+            boxShadow={1}
+            border={"1px #000 solid"}
+            display="inline-block"
+          ></Box>
+          <span style={{ lineHeight: "22px", lineBreak: "anywhere", fontSize: "12px" }}>TODOS</span>
+        </Box>
+        {labelsProvider.map(label => {
+          if (!label.is_deleted) {
+            return (
+              <Box
+                onClick={() => setCurrentPriority(label?.id)}
+                display={"flex"}
+                gap="5px"
+                key={uuid()}
+                style={{ height: "min-content", width: "130px", cursor: "pointer" }}
+              >
+                <Box
+                  style={{
+                    background: label.color,
+                    padding: "2px 4px",
+                    height: "15px",
+
+                    width: "15px",
+                  }}
+                  boxShadow={1}
+                  color={hexToRgb(label.color) > 120 ? "#000000" : "#ffffff"}
+                  display="inline-block"
+                  border={"1px #000 solid"}
+                ></Box>
+                <span style={{ lineHeight: "22px", lineBreak: "anywhere", fontSize: "12px" }}>
+                  {label.title?.toUpperCase()}
+                </span>
+              </Box>
+            )
+          }
+          return <></>
+        })}
+        <Dialog onClose={handleClose} open={openModal}>
+          <Card style={{ padding: "8px", display: "flex", flexDirection: "column" }}>
+            <Stack>
+              <Box component={"span"} key={uuid()} onClick={() => window.location.reload()} borderRadius={1}>
+                <b>Seja bem vindo.</b>
+                <br />
+                <br /> Hum vi que é primeira vez que você esta acessando ou você acabou de logar nossa agenda por este
+                dispositivo.
+                <br /> É só <b onClick={handleClose}>clicar</b> no botão abaixo que vamos buscar toas as informações
+                para você!
+              </Box>
+            </Stack>
+            <Button onClick={handleClose} variant={"contained"} style={{ marginTop: "32px" }}>
+              Vamos lá!
+            </Button>
+          </Card>
+        </Dialog>
+      </Box>
+    </>
   )
 }
--- src/Components/Calendar/Components/Header/Header.tsx
+++ src/Components/Calendar/Components/Header/Header.tsx
@@ -22,7 +22,8 @@ import { useAuth } from "../../../../Provider/Auth/Auth"
 import { LabelForm } from "../Form/LabelForm"
 import { Filter } from "../Filter/Filter"
 import { StatusHandler } from "../../StatusHandler"
-
+import logo from "../../../../assets/logo.png"
+import { useMobile } from "../../../../Provider/Theme/Mobile"
 interface HeaderProps {
   setCurrentPriority: React.Dispatch<React.SetStateAction<number>>
 }
@@ -32,6 +33,7 @@ export const Header = ({ setCurrentPriority }: HeaderProps) => {
   const [anchorEl, setAnchorEl] = React.useState(null)
   const [currentForm, setCurrentForm] = useState("")
   const allUsers = [...adminList, ...memberList]
+  const { mobile } = useMobile()
   const open = Boolean(anchorEl)
   const handleClick = event => {
     setAnchorEl(event.currentTarget)
@@ -71,142 +73,168 @@ export const Header = ({ setCurrentPriority }: HeaderProps) => {
         {forms[currentForm]}
         <Divider />
       </Drawer>
-      <Box width={"100%"} height={"75px"} boxShadow={2} bgcolor={"#0d1f35"} mb={1}>
-        <Stack direction={"row"} width={"100vw"} justifyContent={"space-between"}>
-          <Box bgcolor={"green"} height="75px" width={"75px"} justifySelf={"flex-start"}>
-            LOGO
+      <Box width={"100%"} height={"80px"} boxShadow={2} bgcolor={"#000"} mb={1}>
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <Box display={"flex"}>
+            {!mobile ? (
+              <>
+                <Box
+                  height="80px"
+                  width={"300px"}
+                  justifySelf={"flex-start"}
+                  display="flex"
+                  bgcolor={"#FFF"}
+                  alignItems="center"
+                >
+                  <img src={logo} alt="logo" style={{ paddingLeft: "24px", height: "90%" }} />
+                </Box>
+                <Box color="white" alignSelf={"center"} ml={2} fontSize="32px">
+                  BAND NAME HERE
+                </Box>
+              </>
+            ) : (
+              <Box p="16px">
+                <Box height="30px" width={"50px"} justifySelf={"flex-start"} display="flex" alignItems="center">
+                  <img src={logo} alt="logo" style={{ height: "90%" }} />
+                </Box>
+                <Box color="white" fontSize="12px">
+                  BAND BANE HERE
+                </Box>
+              </Box>
+            )}
           </Box>
           <Stack width={"75px"} height={"75px"} alignItems="center" justifyContent={"center"}>
-            <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }} justifySelf="end">
-              <Tooltip title="Account settings">
-                <IconButton
-                  onClick={handleClick}
-                  size="small"
-                  sx={{ ml: 2 }}
-                  aria-controls={open ? "account-menu" : undefined}
-                  aria-haspopup="true"
-                  aria-expanded={open ? "true" : undefined}
-                >
-                  <MenuIcon sx={{ width: 32, height: 32, color: "white" }}></MenuIcon>
-                </IconButton>
-              </Tooltip>
-            </Box>
-            <Box>
-              <Menu
-                anchorEl={anchorEl}
-                id="account-menu"
-                open={open}
-                onClose={handleClose}
-                onClick={handleClose}
-                PaperProps={{
-                  elevation: 0,
-                  sx: {
-                    overflow: "visible",
-                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                    mt: 1.5,
-                    "& .MuiAvatar-root": {
-                      width: 32,
-                      height: 32,
-                      ml: -0.5,
-                      mr: 1,
+            <>
+              <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }} justifySelf="end">
+                <Tooltip title="Account settings">
+                  <IconButton
+                    onClick={handleClick}
+                    size="small"
+                    aria-controls={open ? "account-menu" : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? "true" : undefined}
+                  >
+                    <MenuIcon sx={{ width: 32, height: 32, color: "white" }}></MenuIcon>
+                  </IconButton>
+                </Tooltip>
+              </Box>
+              <Box>
+                <Menu
+                  anchorEl={anchorEl}
+                  id="account-menu"
+                  open={open}
+                  onClose={handleClose}
+                  onClick={handleClose}
+                  PaperProps={{
+                    elevation: 0,
+                    sx: {
+                      overflow: "visible",
+                      filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                      mt: 1.5,
+                      "& .MuiAvatar-root": {
+                        width: 32,
+                        height: 32,
+                        ml: -0.5,
+                        mr: 1,
+                      },
+                      "&:before": {
+                        content: '""',
+                        display: "block",
+                        position: "absolute",
+                        top: 0,
+                        right: 14,
+                        width: 10,
+                        height: 10,
+                        bgcolor: "background.paper",
+                        transform: "translateY(-50%) rotate(45deg)",
+                        zIndex: 0,
+                      },
                     },
-                    "&:before": {
-                      content: '""',
-                      display: "block",
-                      position: "absolute",
-                      top: 0,
-                      right: 14,
-                      width: 10,
-                      height: 10,
-                      bgcolor: "background.paper",
-                      transform: "translateY(-50%) rotate(45deg)",
-                      zIndex: 0,
-                    },
-                  },
-                }}
-                transformOrigin={{ horizontal: "right", vertical: "top" }}
-                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-              >
-                <MenuItem
-                  onClick={() => {
-                    setCurrentForm("superAdmin")
-                    setOpenDrawer(true)
                   }}
+                  transformOrigin={{ horizontal: "right", vertical: "top" }}
+                  anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
                 >
-                  <Avatar /> Meu Perfil
-                </MenuItem>
-                <Divider />
-                <Typography component="h6" fontSize={"10px"} pl={1}>
-                  Acesso só para administradores
-                </Typography>
-                <MenuItem
-                  onClick={() => {
-                    setCurrentForm("appointment")
-                    setOpenDrawer(true)
-                  }}
-                  disabled={!adm}
-                >
-                  <ListItemIcon>
-                    <EventAvailable fontSize="small" />
-                  </ListItemIcon>
-                  Adicionar compromisso
-                </MenuItem>
-                <MenuItem
-                  onClick={() => {
-                    setCurrentForm("band")
-                    setOpenDrawer(true)
-                  }}
-                  disabled={!adm}
-                >
-                  <ListItemIcon>
-                    <MusicNote fontSize="small" />
-                  </ListItemIcon>
-                  Adicionar/Editar banda
-                </MenuItem>
-                <MenuItem
-                  onClick={() => {
-                    setCurrentForm("admin")
-                    setOpenDrawer(true)
-                  }}
-                  disabled={!superAdmin}
-                >
-                  <ListItemIcon>
-                    <PersonAddAlt fontSize="small" />
-                  </ListItemIcon>
-                  Adicionar administrador
-                </MenuItem>
-                <MenuItem
-                  onClick={() => {
-                    setCurrentForm("label")
-                    setOpenDrawer(true)
-                  }}
-                  disabled={!superAdmin}
-                >
-                  <ListItemIcon>
-                    <ColorLens fontSize="small" />
-                  </ListItemIcon>
-                  Adicionar label
-                </MenuItem>
-                <MenuItem
-                  onClick={() => {
-                    setCurrentForm("status")
-                    setOpenDrawer(true)
-                  }}
-                  disabled={!adm}
-                >
-                  <ListItemIcon>
-                    <SyncOutlined fontSize="small" />
-                  </ListItemIcon>
-                  Atualizar compromissos
-                </MenuItem>
-                <MenuItem onClick={() => signOut()}>
-                  <ListItemIcon>
-                    <Logout fontSize="small" />
-                  </ListItemIcon>
-                  Logout
-                </MenuItem>
-              </Menu>
-            </Box>
+                  <MenuItem
+                    onClick={() => {
+                      setCurrentForm("superAdmin")
+                      setOpenDrawer(true)
+                    }}
+                  >
+                    <Avatar /> Meu Perfil
+                  </MenuItem>
+                  <Divider />
+                  <Typography component="h6" fontSize={"10px"} pl={1}>
+                    Acesso só para administradores
+                  </Typography>
+                  <MenuItem
+                    onClick={() => {
+                      setCurrentForm("appointment")
+                      setOpenDrawer(true)
+                    }}
+                    disabled={!adm}
+                  >
+                    <ListItemIcon>
+                      <EventAvailable fontSize="small" />
+                    </ListItemIcon>
+                    Adicionar compromisso
+                  </MenuItem>
+                  <MenuItem
+                    onClick={() => {
+                      setCurrentForm("band")
+                      setOpenDrawer(true)
+                    }}
+                    disabled={!adm}
+                  >
+                    <ListItemIcon>
+                      <MusicNote fontSize="small" />
+                    </ListItemIcon>
+                    Adicionar/Editar banda
+                  </MenuItem>
+                  <MenuItem
+                    onClick={() => {
+                      setCurrentForm("admin")
+                      setOpenDrawer(true)
+                    }}
+                    disabled={!superAdmin}
+                  >
+                    <ListItemIcon>
+                      <PersonAddAlt fontSize="small" />
+                    </ListItemIcon>
+                    Adicionar administrador
+                  </MenuItem>
+                  <MenuItem
+                    onClick={() => {
+                      setCurrentForm("label")
+                      setOpenDrawer(true)
+                    }}
+                    disabled={!superAdmin}
+                  >
+                    <ListItemIcon>
+                      <ColorLens fontSize="small" />
+                    </ListItemIcon>
+                    Adicionar label
+                  </MenuItem>
+                  <MenuItem
+                    onClick={() => {
+                      setCurrentForm("status")
+                      setOpenDrawer(true)
+                    }}
+                    disabled={!adm}
+                  >
+                    <ListItemIcon>
+                      <SyncOutlined fontSize="small" />
+                    </ListItemIcon>
+                    Atualizar compromissos
+                  </MenuItem>
+                  <MenuItem onClick={() => signOut()}>
+                    <ListItemIcon>
+                      <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Logout
+                  </MenuItem>
+                </Menu>
+              </Box>
+            </>
           </Stack>
         </Stack>
       </Box>
