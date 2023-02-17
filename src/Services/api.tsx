c4cbacfd70eed872787a7099b25d8c8b106b21db
prgMgava <78923115+prgMgava@users.noreply.github.com>
2023-02-17 16:45:35 -0300

    new build

--- src/Services/api.tsx
+++ src/Services/api.tsx
@@ -1,9 +1,9 @@
 import axios from "axios"
 
-export const api = axios.create({
-  baseURL: "http://localhost:3333/",
-})
-
 // export const api = axios.create({
-//   baseURL: "https://www.redbusapi.tech/",
+//   baseURL: "http://localhost:3333/",
 // })
+
+export const api = axios.create({
+  baseURL: "https://www.redbusapi.tech/",
+})
